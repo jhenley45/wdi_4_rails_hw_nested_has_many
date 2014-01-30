4f53429c9389fce87b5bb082c53f9233d9b88310
Jack Henley <jack.d.henley@gmail.com>
2014-01-29 23:47:10 -0500

    All specifications met, HW completed

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,8 +10,21 @@ class UsersController < ApplicationController
 
 
   def create
+  	@user = User.new(user_params)
+  	if @user.save
+  		redirect_to users_path
+  	else
+  		render :new
+  	end
   end
 
   def new
+  	@user = User.new
+  end
+
+  private
+
+  def user_params
+  	params.require(:user).permit(:email, :name)
   end
 end
