e31f66561c8ad806566407dc57cde755d15d0a5c
Jack Henley <jack.d.henley@gmail.com>
2014-01-29 21:18:47 -0500

    Routing completed

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,10 @@
 SharedBlog::Application.routes.draw do
 
-  get "comment/show"
-  get "comment/index"
-  get "comment/create"
-  get "comment/new"
-  get "article/show"
-  get "article/index"
-  get "article/create"
-  get "article/new"
-  get "article/edit"
-  get "user/show"
-  get "user/index"
-  get "user/create"
-  get "user/new"
+  resources :users do
+    resources :articles do
+      resources :comments
+    end
+  end
+
+  root to: 'articles#index'
 end
